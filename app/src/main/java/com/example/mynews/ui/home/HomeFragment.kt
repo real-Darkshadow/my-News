8da8d7f16bd42955c747c1a2d9f779c22f7c50cf
real-Darkshadow <aadarsh.21bcan262@jecrcu.edu.in>
2023-02-07 23:19:23 +0530

    chp group added to home fragment

--- app/src/main/java/com/example/mynews/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/mynews/ui/home/HomeFragment.kt
@@ -35,7 +35,7 @@ class HomeFragment : Fragment() {
         val homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
-        binding.
+        binding.chgrp.
         getapidata(cat = )
 
 
